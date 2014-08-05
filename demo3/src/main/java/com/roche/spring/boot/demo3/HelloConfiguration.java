90ac200a0310926da6ea02f1c2bee0bc545d81d5
Krzysztof Koziol <krzysztof.koziol@roche.com>
2014-08-05 15:58:21 +0200

    initial samples commit

---
+++ demo3/src/main/java/com/roche/spring/boot/demo3/HelloConfiguration.java
@@ -0,0 +1,18 @@
+package com.roche.spring.boot.demo3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+
+@EnableAutoConfiguration
+@ComponentScan
+public class HelloConfiguration {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(HelloConfiguration.class, args);
+	}
+	
+}
+
+
+// mvn package && java -jar target\demo2-0.0.1-SNAPSHOT.jar
---
+++ demo3/src/main/java/com/roche/spring/boot/demo3/HelloController.java
@@ -0,0 +1,16 @@
+package com.roche.spring.boot.demo3;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+	@RequestMapping("/")
+	@ResponseBody
+	String home() {
+		return "Hello World!";
+	}
+
+}
